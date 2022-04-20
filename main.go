06af8872f3731da3aaf754fe6a7c62e065456788
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 01:00:25 +0700

    type declaration

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "log"
+
 func main() {
+
+	type NOBP string
+	type IS_STUDENT bool
+
+	var noBp NOBP = "1610140"
+	log.Println(noBp)
+
+	var isStudent IS_STUDENT = false
+	log.Println(isStudent)
 
 }
