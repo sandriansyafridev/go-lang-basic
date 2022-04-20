f0839329760bbc1dc5f3c6ad0cf76b74c0e24917
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 01:18:59 +0700

    boolean operation

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "log"
+
 func main() {
+
+	nilaiUts := 70
+	nilaiUas := 80
+
+	if nilaiUts > 75 && nilaiUas > 75 {
+		log.Println("PASS EXAM")
+	} else {
+		log.Println("NOT PASS EXAM")
+	}
 
 }
