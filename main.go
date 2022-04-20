3009cee479bbac3c8a0aee16f3fd10a7ecc456b6
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 06:37:15 +0700

    anonymous function

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "log"
+
+func displayName(value string, filter func(value string) string) string {
+	return filter(value)
+}
+
 func main() {
+	result := displayName("Sandrian", func(value string) string {
+		switch {
+		case value != "anjing":
+			return value
+		default:
+			return "..."
+		}
+	})
+
+	log.Println(result)
 
 }
