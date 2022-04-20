59b5ff6205833bb9bee9ea6899b38b232a8e58a8
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 04:32:53 +0700

    function return multiple value

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "log"
+
+func getFullName() (string, string) {
+	return "Sandrian", "Syafri"
+}
+
 func main() {
+
+	firstName, lastName := getFullName()
+	log.Println(firstName, lastName)
 
 }
