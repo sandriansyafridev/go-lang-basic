3652f432003c7dcf1ed0a047ec5487b82c6d8a45
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 06:56:39 +0700

    defer, panic, recover

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import "log"
+
+func initApp() {
+	log.Println("START RUNNING APPLICATION")
+
+}
+
+func runApp(err bool) {
+	if err {
+		panic("SOMETHING WRONG")
+	}
+
+	log.Println("RUNNING APPLICATION")
+	log.Println("RUNNING APPLICATION")
+	log.Println("RUNNING APPLICATION")
+	log.Println("RUNNING APPLICATION")
+
+}
+
+func endApp() {
+	errorMessage := recover()
+
+	log.Println("FINISH RUNNING APPLICATION")
+	log.Println(errorMessage)
+}
+
 func main() {
+
+	defer endApp() // dieksekusi setelah semua proses selesai.
+
+	initApp()
+	runApp(true)
 
 }
