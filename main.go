6fe89a9c86123f8cbb8ddf73e09c55724eccc53e
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 04:49:08 +0700

    function as params

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "log"
+
+func filterName(value string) string {
+	switch {
+	case value != "anjing":
+		return value
+	default:
+		return "..."
+	}
+}
+
+func displayName(value string, filterName func(string) string) string {
+	return filterName(value)
+}
+
 func main() {
+
+	result1 := displayName("anjing", filterName)
+	result2 := displayName("Sandrian Syafri", filterName)
+	log.Println(result1)
+	log.Println(result2)
 
 }
