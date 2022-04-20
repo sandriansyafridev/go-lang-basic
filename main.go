e16f52e94e21fc6782f783a514a9cdb5343f2738
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 04:40:50 +0700

    function variadic

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "log"
+
+func sumNumbers(numbers ...int) (result int) {
+
+	for _, number := range numbers {
+		result += number
+	}
+
+	return result
+
+}
+
 func main() {
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7}
+
+	result := sumNumbers(numbers...)
+	log.Println(result)
 
 }
