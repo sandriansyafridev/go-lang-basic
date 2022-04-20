15ed53e0e7df980711756a206e5360a77f0ba544
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 03:53:44 +0700

    function return value

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
+func sayHello(name string) string {
+	return fmt.Sprintf("Hello %s", name)
+}
+
 func main() {
+
+	displaySayHello := sayHello("Sandrian Syafri")
+	log.Println(displaySayHello)
 
 }
