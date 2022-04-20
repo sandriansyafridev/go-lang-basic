8b1aba6988d3431e3a2a394585540e4dcc9c6fc1
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 01:53:42 +0700

    map

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "log"
+
 func main() {
 
+	//create map value
+	mapString := map[string]string{
+		"first_name": "Sandrian",
+		"last_name":  "Syafri",
+		"address":    "Jambi",
+	}
+
+	mapString["first_name"] = "edited" // update map value
+	log.Println(mapString, "len map before delete =", len(mapString))
+
+	delete(mapString, "address")
+	log.Println(mapString)
+	log.Println(mapString, "len map after delete =", len(mapString))
 }
