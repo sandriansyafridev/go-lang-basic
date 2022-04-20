4574489d3f3c769c089e902b8caeae51b50cbf6f
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 00:57:03 +0700

    convertion type data

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "log"
+
 func main() {
+
+	var number1 int32 = 100
+	var number2 int64 = int64(number1)
+	log.Println(number1)
+	log.Println(number2)
+
+	var name string = "Sandrian Syafri"
+	var index1 string = string(name[0])
+	log.Println(name)
+	log.Println(index1)
 
 }
