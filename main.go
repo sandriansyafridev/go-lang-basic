bb601f4e02a27c2c1babe6a30dc5c17b4e13914a
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 00:47:03 +0700

    variable

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import "log"
+
 func main() {
+
+	// cara pertama
+	var name string
+	name = "Sandrian"
+	name = "Sandrian Syafri"
+	log.Println(name)
+
+	//cara kedua
+	var address string = "Jambi"
+	log.Println(address)
+
+	// cara ketiga
+	telepon := "08xxx"
+	log.Println(telepon)
+
+	//cara ke empat
+	var (
+		email    = "sandriansyafri.dev@gmail.com"
+		password = "password"
+	)
+	log.Println(email)
+	log.Println(password)
 
 }
