efb125e66cfad9769c6915846fc91a3423a8cf22
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 01:58:10 +0700

    if expression

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "log"
+
 func main() {
+
+	name := "Sandrian Syafri"
+
+	// cara satu - if else if
+	if name == "Sandrian Syafri" {
+		log.Println("Hello", name)
+	} else if name == "Hafid" {
+		log.Println("Hi", name)
+	} else {
+		log.Println("No found!")
+	}
+
+	// cara dua - if with short statment
+	if len := len(name); len > 5 {
+		log.Println("Besar dari 5")
+	} else {
+		log.Println("Kecil dari 5")
+	}
 
 }
