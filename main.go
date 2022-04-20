a55accd4f390fc41b266565025e9927811d81ac4
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 01:11:25 +0700

    boolean operation

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "log"
+
 func main() {
+
+	x := 10
+	y := 11
+
+	log.Println("===========")
+	log.Println("x =", x)
+	log.Println("y =", y)
+	log.Println("===========")
+	log.Println("x == y =", x == y)
+	log.Println("x != y =", x != y)
+	log.Println("x > y =", x > y)
+	log.Println("x >= y =", x >= y)
+	log.Println("x < y =", x < y)
+	log.Println("x <= y =", x <= y)
 
 }
