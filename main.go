055fc84fc2abad1341eb7fab438c8139ed86c364
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 04:34:43 +0700

    function return named value

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "log"
+
+func getFullName() (firstName string, lastName string) {
+
+	firstName = "Sandrian"
+	lastName = "Syafri"
+
+	return
+}
+
 func main() {
+
+	firstName, lastName := getFullName()
+	log.Println(firstName, lastName)
 
 }
