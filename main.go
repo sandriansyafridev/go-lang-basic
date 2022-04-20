fa2f8759fec1e9899b28d72dfaefe50728f57e2e
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 00:52:52 +0700

    const

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "log"
+
 func main() {
+
+	// cara satu - inline declaration
+	const APP = "Go APP"
+	const VERSION = "1.0.0"
+	log.Println(APP, VERSION)
+
+	//cara dua - multiple declaration
+	const (
+		FIRST_NAME = "Sandrian"
+		LAST_NAME  = "Syafri"
+	)
+	log.Println(FIRST_NAME, LAST_NAME)
 
 }
