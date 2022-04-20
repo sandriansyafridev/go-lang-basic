ac1cca9d204c7270ba77686c074ee7e4aa2c8814
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 03:47:26 +0700

    break & continue statment

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import "log"
+
 func main() {
+
+	// break statment
+	for i := 1; i <= 10; i++ {
+		log.Println(i)
+		if i == 5 {
+			break
+		}
+	}
+
+	log.Println("=======================") // end break statment
+
+	//continue stetment
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+
+		log.Println(i)
+	}
 
 }
