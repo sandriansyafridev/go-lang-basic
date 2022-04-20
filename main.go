30b5046b8840b1970ad582109e3c57f1922f3348
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 03:41:41 +0700

    for loops

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import "log"
+
 func main() {
+
+	// cara pertama
+	number := 1
+	for number <= 10 {
+		log.Println(number)
+		number++
+	}
+
+	log.Println("================================") //end cara pertama
+
+	//cara kedua
+	for i := 1; i <= 10; i++ {
+		log.Println(i)
+	}
+
+	items := []string{
+		"item-1",
+		"item-2",
+		"item-3",
+		"item-4",
+	}
+
+	log.Println("================================") //end cara kedua
+
+	for _, item := range items {
+		log.Println(item)
+	}
 
 }
