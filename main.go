b2e9e2733cd72d47e139cded4d87746abfc8f0dd
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 08:28:26 +0700

    039-pointer-function

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "log"
+
+type Person struct {
+	Name string
+}
+
+func UpdateName(person *Person) {
+	person.Name = "Edited"
+	log.Println(person.Name)
+}
+
 func main() {
+
+	person := Person{"Hafid"}
+	UpdateName(&person)
+
+	log.Println(person.Name)
 
 }
