282e8fed91bc8fc385675f0d57809cb6f1737c5a
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 08:23:24 +0700

    pointer

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "log"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
+
+	person1 := Person{"Sandrian", 23}
+	person2 := &person1
+
+	*person2 = Person{"Hafid", 24}
+
+	log.Println(person1)
+	log.Println(person2)
 
 }
