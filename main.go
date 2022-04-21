7029a1043dc8e1aaea3e94611abdebda4c3155e2
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 08:08:49 +0700

    type assertions

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "log"
+
+func DisplayAnything() interface{} {
+	return 10
+}
+
 func main() {
 
+	result := DisplayAnything()
+
+	switch value := result.(type) {
+	case string:
+		log.Println(value)
+	default:
+		log.Println("not string")
+	}
 }
