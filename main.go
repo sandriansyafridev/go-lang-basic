7f5ea728a2c6a2955218b5209d5c43088b571f0a
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 08:48:39 +0700

    reset

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import "log"
-
-type Person struct {
-	Name string
-}
-
-func (person *Person) UpdateName(value string) {
-	person.Name = value
-}
-
 func main() {
-
-	person := Person{"Hafid"}
-
-	log.Println(person)
-	person.UpdateName("Hafid edited!")
-	log.Println(person)
 
 }
