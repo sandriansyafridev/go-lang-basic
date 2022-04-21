8165b44c8c94a34c1e280e5b63146d62d25bf8a0
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 07:21:24 +0700

    struct method

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "log"
+
+type Person struct {
+	Name string
+}
+
+func (person Person) SayHello() {
+	log.Println(person.Name)
+}
+
 func main() {
+
+	person := Person{"Sandrian"}
+	person.SayHello()
 
 }
