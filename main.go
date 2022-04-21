405d1cc72a12296937620b6faa141fb4dc6f80ae
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 08:44:46 +0700

    afds

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "log"
+
+type Person struct {
+	Name string
+}
+
+func (person *Person) UpdateName(value string) {
+	person.Name = value
+}
+
 func main() {
+
+	person := Person{"Hafid"}
+
+	log.Println(person)
+	person.UpdateName("Hafid edited!")
+	log.Println(person)
 
 }
