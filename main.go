9f2a5c8dd4c6d905be46cd4a7a5e0613800b71b0
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 07:54:35 +0700

    nil

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "log"
+
+func DisplayName(name string) interface{} {
+	if name == "" {
+		return nil
+	}
+
+	return name
+}
+
 func main() {
+
+	if result := DisplayName(""); result != nil {
+		log.Println(result)
+	}
+
+	log.Println("no value")
 
 }
