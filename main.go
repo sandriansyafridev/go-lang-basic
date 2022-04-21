9fcae5aab017417849fdbfb99357443557b67f96
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 07:43:18 +0700

    interface

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import "log"
+
+type HasName interface {
+	GetName()
+}
+
+type Person struct {
+	Name string
+}
+
+type Animal struct {
+	Name string
+}
+
+func (person Person) GetName() {
+	log.Println(person.Name)
+}
+
+func (animal Animal) GetName() {
+	log.Println(animal.Name)
+}
+
+func DisplayName(f HasName) {
+	f.GetName()
+}
+
 func main() {
+
+	person := Person{"Sandrian"}
+	animal := Animal{"Cat"}
+
+	DisplayName(person)
+	DisplayName(animal)
 
 }
