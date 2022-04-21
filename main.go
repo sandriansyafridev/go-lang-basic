fe8bb846cdc9bb807e8fc5bf566b44ebc2206c0e
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-21 08:00:10 +0700

    errors

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"errors"
+	"log"
+)
+
+func Devide(pembilang int, pembagi int) (int, error) {
+	if pembagi == 0 {
+		return 0, errors.New("cannot devide")
+	}
+
+	result := pembilang / pembagi
+	return result, nil
+}
+
 func main() {
+
+	result, err := Devide(10, 1)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	log.Println(result)
 
 }
